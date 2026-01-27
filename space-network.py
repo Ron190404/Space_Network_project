fcc87efec93fee5874bbe4e768da0f1aad48780d
Ron <roneva1909@gmail.com>
2026-01-27 12:28:36 +0100

    level_7_final

--- space-network.py
+++ space-network.py
@@ -1,4 +1,8 @@
+from main import message
 from space_network_lib import Packet, SpaceNetwork, SpaceEntity
+
+class SecurityBreachError(Exception):
+    pass
 
 
 
@@ -24,23 +28,38 @@ class RelayPacket(Packet):
         return (f"RelayPacket (Relaying {self.packet_to_relay.data} "
                 f"to {self.packet_to_relay.receiver}) from {self.sender}")
 
+class EncryptedPacket(Packet):
+    def __init__(self, data, sender,receive,key):
+        super().__init__(data,sender,receive)
+        self.key = key
+
+    def decrypt(self,key):
+        if key == self.key:
+            return self.data
+        else:
+            raise SecurityBreachError("invalid key")
+
 
 
 class RelaySatellite(SpaceEntity):
     def receive_signal(self, packet):
         if isinstance(packet, RelayPacket):
 
-            print(f"Unwrapping and forwarding to {packet.packet_to_relay.receiver}")
-            inner_packet = packet.packet_to_relay
-            attempt_transmission(self.network, inner_packet)
+            print(f"relaying toward {packet.packet_to_relay.receiver}")
+            attempt_transmission(self.network, packet.packet_to_relay)
+        elif isinstance(packet, EncryptedPacket):
+            try:
+                secret_message = packet.decrypt("1234")
+                print(f"Final destination reached (secure): {secret_message}")
+            except SecurityBreachError :
+                print("ALERTE ")
         else:
-
             print(f"Final destination reached: {packet.data}")
 
 def smart_send_packet(network, current, path):
 
 
-        current_packet = Packet(current, path[-2], path[-1])
+        current_packet = EncryptedPacket(current,path[-2],path[-1], "1234")
 
         for i in range(len(path) - 3, -1, -1):
 
